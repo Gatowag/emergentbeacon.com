3a72243c962402ad880a1fd46737ab807165b3c1
Brandt Hughes <brandt@brandthughes.com>
2025-07-08 23:52:22 -0500

    bandcamp embed function
    added a function to update the colors of a bandcamp embed if it exists

--- color-palettes.js
+++ color-palettes.js
@@ -9,13 +9,27 @@ themeSwitch.addEventListener("click", () => {
 	let palette = localStorage.getItem("palette");
 
 	if (palette == "dark") {
-		enableLight()
+		enableLight();
+		updateBandcamp(".mode-light");
 	} else if (palette == "light") {
 		enableContrast()
+		updateBandcamp(".mode-contrast");
 	} else {
 		enableDark()
+		updateBandcamp(":root");
 	}
 })
+
+function updateBandcamp(b) {
+	let bcamp = document.getElementById("bandcamp-player");
+	if (bcamp !== null) {
+		let p = document.querySelector(b);
+		let s = bcamp.src;
+		let lColor = getComputedStyle(p).getPropertyValue('--bcampLink').slice(1);
+		let a = s.indexOf("/linkcol=");
+		bcamp.src = (s.slice(0, a + 9) + lColor + s.slice(a + 9 + 6, -1));
+	}
+}
 
 function enableLight() {
 	document.body.classList.add("mode-light");

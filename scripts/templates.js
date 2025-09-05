78e9e97bdc86dfd5f33e31c43a5624a4faeda5c8
Brandt Hughes <brandt@brandthughes.com>
2025-09-05 02:10:13 -0500

    ported youtube embed code to template.js
    Since it's always the same, it made sense to me to set these functions up so I can just declare the video's ID and not worry about the rest on a page-by-page basis.

--- scripts/templates.js
+++ scripts/templates.js
@@ -113,6 +113,45 @@ function buildFooter(path) {
 		</socials>`;
 }
 
+
+/* [### ↓ YT EMBED GENERATION ↓ ###] */
+// This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// This function creates an <iframe> (and YouTube player) after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+	player = new YT.Player('youtube', {
+		height: '400',
+		width: '800',
+		videoId: ytID,
+		playerVars: {
+			'playsinline': 1
+		},
+		events: {
+			'onReady': onPlayerReady,
+			'onStateChange': onPlayerStateChange
+		}
+	});
+}
+
+// The API will call this function when the video player is ready.
+function onPlayerReady(event) {}
+function onPlayerStateChange(event) {}
+
+// generic seekTo function taking a player element and seconds as parameters
+function playerSeekTo(player, seconds) {
+	player.seekTo(seconds);
+	player.scrollIntoView({
+		behavior: 'smooth',
+		block: 'start',
+		inline: 'nearest'
+	});
+}
+
 function relativePath(levels) {
 	let a = "";
 	for (l = levels; l > 0; l--) { a += "." }
